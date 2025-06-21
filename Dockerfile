0957dc6ee90556f3c1a515a199fd5e76c6eb3045
Biplove82 <biplavmandal.mandal@gmmail.com>
2025-06-22 00:55:36 +0530

    get user

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
 FROM node:16-alpine
+
 WORKDIR /CUET_PLUS
+
 COPY package.json ./
 RUN npm install
+
 COPY . .
-ENV DB_URL=${DB_URL}
 
-ENV PASSKEY=${PASSKEY}
-ENV PORT=${PORT}
+EXPOSE 3000
 
-
-EXPOSE 3030
-
-CMD ["node", "src/index.js"]
+CMD ["npm", "start"]
--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,7 +1,8 @@
 var router =require('express').Router();
 
-const { userRegister }=require("../Controllers/userController");
+const { userRegister, getalluser }=require("../Controllers/userController");
 
 router.post('/register', userRegister); 
+router.get("/get-user",getalluser);
 
 module.exports = router;
